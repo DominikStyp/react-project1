58e0b090bba297758f27c244b6061c9bdc867d81
DominikStyp <dominik.stypula1@gmail.com>
2019-12-19 00:21:45 +0100

    1) class based components, constructor 2) introduction to state

--- my-first-app/src/DominikComponents/MyList.js
+++ my-first-app/src/DominikComponents/MyList.js
@@ -3,20 +3,27 @@ import '../DominikComponents/MyList.css';
 import OneItem from "./OneItem";
 import todosData from "./todosData";
 
+class MyList extends React.Component {
 
-function MyList() {
-    let items = getItems(todosData);
-    return (
-        <ul className="myUl">
-            {items}
-        </ul>
-    );
-}
+    constructor(){
+        super();
+        this.state = {
+            something: "That is something!"
+        }
+    }
 
-function getItems(arr)
-{
-    // here we use arrow function (ES 6)
-    return arr.map(item => <OneItem desc={item.desc} checked={item.checked}/>)
+    render() {
+        let items = this.getItems(todosData);
+        return (
+            <ul className="myUl">
+                {items}
+            </ul>
+        );
+    }
+    getItems(arr) {
+        // here we use arrow function (ES 6)
+        return arr.map(item => <OneItem key={item.id} desc={item.desc} checked={item.checked}/>)
+    }
 }
 
 export default MyList;
--- my-first-app/src/DominikComponents/OneItem.js
+++ my-first-app/src/DominikComponents/OneItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import '../DominikComponents/OneItem.css';
-function OneItem(props) {
-    const cName = (props.checked) ? "checked" : "unchecked" ;
-    return (
-        <li className={cName} >
-            {props.desc} <input type="checkbox" checked={props.checked}/>
-        </li>
-    );
+
+class OneItem extends React.Component {
+    liClicked(){
+        console.log("clicked");
+    }
+
+    render() {
+        const cName = (this.props.checked) ? "checked" : "unchecked" ;
+        return (
+            <li className={cName} onClick={this.liClicked} >
+                {this.props.desc}
+                <input type="checkbox" checked={this.props.checked} readOnly={true}/>
+            </li>
+        );
+    }
 }
 
 OneItem.defaultProps = {
