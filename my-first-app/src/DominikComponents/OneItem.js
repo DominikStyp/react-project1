f4008bea35e48bd299aa6df7326139055d0ae63d
DominikStyp <dominik.stypula1@gmail.com>
2019-12-19 22:03:43 +0100

    1) setting new state/getting previous state 2) binding methods that use state to the current component 3) using props in the constructor()

--- my-first-app/src/DominikComponents/OneItem.js
+++ my-first-app/src/DominikComponents/OneItem.js
@@ -2,21 +2,46 @@ import React from "react";
 import '../DominikComponents/OneItem.css';
 
 class OneItem extends React.Component {
-    liClicked(){
-        console.log("clicked");
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            isChecked: this.props.checked
+        };
+        // we must bind method to "this" object
+        // in order to use a state inside of it
+        this.handleLiClick = this.handleLiClick.bind(this);
+    }
+
+    handleLiClick(){
+        // here we pass the previous version of state
+        // WARNING! don't use this.state as previous version of state
+        // it might cause errors!
+        this.setState( (previousState) => {
+            return {
+                isChecked: !previousState.isChecked
+            }
+        });
+    }
+
+    // part of lifecycle
+    componentDidMount() {
+
     }
 
     render() {
-        const cName = (this.props.checked) ? "checked" : "unchecked" ;
+        const cName = (this.state.isChecked) ? "checked" : "unchecked" ;
         return (
-            <li className={cName} onClick={this.liClicked} >
+            <li className={cName} onClick={this.handleLiClick} >
                 {this.props.desc}
-                <input type="checkbox" checked={this.props.checked} readOnly={true}/>
+                <input type="checkbox" checked={this.state.isChecked} />
             </li>
         );
     }
 }
 
+// this is how we set up default props, those are used if no value is passed to component
 OneItem.defaultProps = {
     checked: false,
     desc: "[UNDEFINED]"
