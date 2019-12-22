8fd641a16f68336b1f2ac2c52296afb3c16b8654
DominikStyp <dominik.stypula1@gmail.com>
2019-12-22 18:31:13 +0100

    App refactored to class from function

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyList from "./DominikComponents/MyList";
-import People from "./People";
+// import MyList from "./DominikComponents/MyList";
+// import People from "./DominikComponents/People";
+import MyForm from "./DominikComponents/MyForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <People />
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor(){
+        super();
+    }
+
+    render(){
+          return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+               <MyForm />
+              </header>
+            </div>
+          );
+    }
 }
 
 export default App;
